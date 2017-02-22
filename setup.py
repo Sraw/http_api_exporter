ddcacbd265ee8238b617236b67de39abd6965071
Sraw <lzyl888@gmail.com>
2017-02-22 06:04:18 +0000

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from distutils.core import setup
+
 setup(
   name = 'http_api_exporter',
-  packages = ['http_api_exporter'], # this must be the same as the name above
+  packages = ['http_api_exporter'],
   version = '0.1',
   description = 'A simple api exporter for py',
   author = 'Sraw',
   author_email = 'lzyl888@gmail.com',
-  url = 'https://github.com/Sraw/http_api_exporter', # use the URL to the github repo
-  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/Sraw/http_api_exporter', 
+  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/0.1', 
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  classifiers = [
+      'Programming Language :: Python :: 3'
+    ],
+  install_requires=[
+          'tornado',
+      ],
 )

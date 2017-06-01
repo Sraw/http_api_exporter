b9caf4da6b9be55b3e884611c220a375baf6b7cc
Sraw <lzyl888@gmail.com>
2017-06-01 05:18:46 +0000

    update to a new incompatible stable version v1.0.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'http_api_exporter',
   packages = ['http_api_exporter'],
-  version = '1.0.3',
+  version = '1.0.0',
   description = 'A simple api exporter for py',
   author = 'Sraw',
   author_email = 'lzyl888@gmail.com',
   url = 'https://github.com/Sraw/http_api_exporter', 
-  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/v1.0.3', 
+  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/v1.0.0', 
   keywords = ['http', 'web', 'api', 'export'], 
   classifiers = [
       'Programming Language :: Python :: 3'

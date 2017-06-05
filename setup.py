f2a7bcc7676e92693aa26475d026205eec44ab0a
Sraw <lzyl888@gmail.com>
2017-06-05 03:04:28 +0000

    update to v1.0.6 adjust structure

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'http_api_exporter',
   packages = ['http_api_exporter'],
-  version = '1.0.5',
+  version = '1.0.6',
   description = 'A simple api exporter for py',
   author = 'Sraw',
   author_email = 'lzyl888@gmail.com',
   url = 'https://github.com/Sraw/http_api_exporter', 
-  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/v1.0.5', 
+  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/v1.0.6', 
   keywords = ['http', 'web', 'api', 'export'], 
   classifiers = [
       'Programming Language :: Python :: 3'

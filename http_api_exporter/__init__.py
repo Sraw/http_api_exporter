9b2b3cf0db8fe329f8597ea9b9e296f1b221771c
Sraw <lzyl888@gmail.com>
2017-06-08 06:53:44 +0000

    fix a bug about disable retry

--- http_api_exporter/__init__.py
+++ http_api_exporter/__init__.py
@@ -48,8 +48,9 @@ class ApiHttpServer:
         if checkListen is None :
             if retry > 0:
                 self.__logger.warning("All retries failed.")
-                raise socket.error("Port {0} to {1} have been used.".format(port, port + retry))
-                
+                raise socket.error("Port {0} to {1} have been used, please change your port.".format(port, port + retry))
+            else:
+                raise socket.error("Port {0} has been used, please consider to enable retry or change your port".format(port))
         tornado.ioloop.IOLoop.current().start()
         
     def __make_app(self):

d3dc960951788fd260bdca4328f0f69a7bb02692
Sraw <lzyl888@gmail.com>
2017-06-08 06:41:12 +0000

    allow no request.body

--- http_api_exporter/Handler.py
+++ http_api_exporter/Handler.py
@@ -21,29 +21,33 @@ class MainHandler(tornado.web.RequestHandler):
         
         logger.info('A coming request at route "%s".' % (self.request.uri))
         
-        try:
-            form = json.loads(self.request.body.decode('utf8'))
-        except Exception as e:
-            self.set_status(400)
-            ErrorMsg = self.__getErrorMsg("Body parse error, only JSON is accepted.")
-            logger.error(traceback.format_exc())
-            self.finish(ErrorMsg)
-            return
-        
-        if 'Input' in form:
-            Input = copy.deepcopy(form['Input'])
-            del form['Input']
-            ArgsDict = form
+        if self.request.body is not None:
+            try:
+                form = json.loads(self.request.body.decode('utf8'))
+            except Exception as e:
+                self.set_status(400)
+                ErrorMsg = self.__getErrorMsg("Body parse error, only JSON is accepted.")
+                logger.error(traceback.format_exc())
+                self.finish(ErrorMsg)
+                return
+            
+            if 'Input' in form:
+                Input = copy.deepcopy(form['Input'])
+                del form['Input']
+                ArgsDict = form
+            else:
+                Input = list()
+                ArgsDict = form
+                
+            if not isinstance(Input, list):
+                self.set_status(400)
+                ErrorMsg = self.__getErrorMsg("Input must be a list.")
+                logger.error("'Input' in the coming request is not a list.")
+                self.finish(ErrorMsg)
+                return
         else:
             Input = list()
-            ArgsDict = form
-            
-        if not isinstance(Input, list):
-            self.set_status(400)
-            ErrorMsg = self.__getErrorMsg("Input must be a list.")
-            logger.error("'Input' in the coming request is not a list.")
-            self.finish(ErrorMsg)
-            return
+            ArgsDict = dict()
         
         try:    
             result = self.__Function(*Input, **ArgsDict)

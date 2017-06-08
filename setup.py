936018a8cf19727549af5d68ddd32b74b48aedf7
Sraw <lzyl888@gmail.com>
2017-06-08 06:55:01 +0000

    update to v1.1.0

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'http_api_exporter',
   packages = ['http_api_exporter'],
-  version = '1.0.7',
+  version = '1.1.0',
   description = 'A simple api exporter for py',
   author = 'Sraw',
   author_email = 'lzyl888@gmail.com',
   url = 'https://github.com/Sraw/http_api_exporter', 
-  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/v1.0.7', 
+  download_url = 'https://github.com/Sraw/http_api_exporter/tarball/v1.1.0', 
   keywords = ['http', 'web', 'api', 'export'], 
   classifiers = [
       'Programming Language :: Python :: 3'
